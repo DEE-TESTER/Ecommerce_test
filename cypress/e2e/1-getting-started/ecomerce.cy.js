95601f215e412e1f2086d517ae9db29b2bc35d04
DEE-TESTER <dolapoaina97@gmail.com>
2024-07-15 10:32:57 +0100

    Add new files

---
+++ cypress/e2e/1-getting-started/ecomerce.cy.js
@@ -0,0 +1,15 @@
+///<reference types="cypress" />
+describe('E-commerce Automation Test', () => {
+    it('Searches for a product', () => {
+      // Visit the website
+      cy.visit('https://ecommerce-playground.lambdatest.io/');
+      //what i did here was to get the CSS locator and type in the text field
+      cy.get('input[placeholder="Search For Products"]').first().type('Smartphone');
+      //here i found the class locator and ckick on it
+      cy.get('.type-text').click()
+      
+
+
+
+          })
+})
